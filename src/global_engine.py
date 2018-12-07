143895f86f58ad325e0276d246ca1eb0be9a392e
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2018-12-07 21:59:16 +0800

    Add state saving

--- src/global_engine.py
+++ src/global_engine.py
@@ -3,18 +3,32 @@ import curses
 from engine import TetrisEngine
 
 
+INIT_ENGINE_STATE = {
+    "KO": 0,
+    "reward": 0,
+    "lines_sent": 0,
+    "hold_block": None,
+    "holded": False,
+    "bomb_lines": 0
+}
+
+
 class GlobalEngine:
     def __init__(
-        self, width, height, player_num
+        self, width, height, player_num, count_down=120
     ):
         self.width = width
         self.height = height
         self.player_num = player_num
+        self.count_down = 120
 
         self.engines = {}
         for i in range(player_num):
             self.engines[i] = TetrisEngine(width, height)
             self.engines[i].clear()
+
+        self.global_state = {}
+        self.engine_states = {}
 
     def play_game(self):
         stdscr = curses.initscr()
@@ -25,15 +39,18 @@ class GlobalEngine:
         done = False
 
         player_actions = {}
+        # Initialization
         for i in range(self.player_num):
             # Initial rendering
             stdscr.addstr(str(self.engines[i]))
+            self.engine_states[i] = INIT_ENGINE_STATE.copy()
             # Initialize dbs
             dbs[i] = []
             # Global action
             player_actions[i] = 6
 
-        while not done:
+        while self.count_down > 0:
+            self.count_down -= 1
             action = 6
             key = stdscr.getch()
 
@@ -55,18 +72,27 @@ class GlobalEngine:
             stdscr.clear()
             for idx, engine in self.engines.items():
                 # Game step
-                state, reward, done = engine.step(action)
+                state, reward, done, cleared_lines = engine.step(action)
+                self.engine_states[idx]['lines_sent'] += cleared_lines
+                for other_idx, other_engine in self.engines.items():
+                    if other_idx != idx:
+                        other_engine.receive_bomb_lines(cleared_lines)
+                        self.engine_states[other_idx]['bomb_lines'] = other_engine.bomb_lines
+
+                self.engine_states[idx]['reward'] += reward
+                self.engine_states[idx]['KO'] += int(done)
                 dbs[idx].append((state, reward, done, action))
 
                 # Render
                 stdscr.addstr(str(engine))
-                stdscr.addstr(f'reward: reward\n')
+                stdscr.addstr(f'reward: {self.engine_states[idx]}\n')
+            stdscr.addstr(f'Time: {self.count_down}\n')
 
         return dbs
 
 
 if __name__ == '__main__':
-    width, height = 10, 20  # standard tetris friends rules
+    width, height = 10, 16  # standard tetris friends rules
     player_num = 2
     global_engine = GlobalEngine(width, height, player_num)
     dbs = global_engine.play_game()
