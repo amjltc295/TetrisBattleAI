1131dff8880bb7235ee5e740493bc1ca9c2d41c6
Patrick Alexander Gersch <p.gersch@campus.tu-berlin.de>
2018-12-17 16:23:40 +0800

    Version for up to 4 player, update_screen() and last_gui_input()

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -5,18 +5,20 @@ import pygame
 from src.global_engine import GlobalEngine
 from src.gui.mino import Tetrimino
 
+running_gui = None
 
 pygame.init()
 
 
 class UIVariables:
-    block_size = 17  # Height, width of single block
+    block_size = 20  # Height, width of single block
     framerate = 30  # Bigger -> Slower
+    shape_names = ['T', 'J', 'L', 'Z', 'S', 'I', 'O']
 
     # Fonts
-    font_path = "./assets/fonts/OpenSans-Light.ttf"
-    font_path_b = "./assets/fonts/OpenSans-Bold.ttf"
-    font_path_i = "./assets/fonts/Inconsolata/Inconsolata.otf"
+    font_path = "gui/fonts/OpenSans-Light.ttf"
+    font_path_b = "gui/fonts/OpenSans-Bold.ttf"
+    font_path_i = "gui/fonts/Inconsolata/Inconsolata.otf"
 
     h1 = pygame.font.Font(font_path, 50)
     h2 = pygame.font.Font(font_path, 30)
@@ -61,14 +63,14 @@ class GUI:
         self.game_over = False
         self.done = False
 
-        self.screen_width = 600
+        self.screen_width = global_state.player_num * 300
         self.screen_height = 400
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.time.set_timer(pygame.USEREVENT, UIVariables.framerate * 10)
         pygame.display.set_caption("Make Tetris Great Again")
 
     # Draw block
-    def draw_block(self, x, y, color):
+    def _draw_block(self, x, y, color):
         pygame.draw.rect(
             self.screen,
             color,
@@ -81,37 +83,32 @@ class GUI:
             1
         )
 
-    def global_to_gui_number(self, number):
-        if number <= 6:
-            return number
-        else:
-            return 6
-
     # Draw board of one player
-    def draw_board(self, x_start, y_start, player_id):
+    def _draw_board(self, x_start, y_start, player_id):
         board = self.global_state.engines[player_id].board
         for x in range(self.global_state.width):
             for y in range(self.global_state.height):
-                dx = x_start + 96 + UIVariables.block_size * x
-                dy = y_start + 0 + UIVariables.block_size * y
-                self.draw_block(dx, dy, UIVariables.t_color[board[x][y + 1]])
+                dx = x_start + 100 + UIVariables.block_size * x
+                dy = y_start + 60 + UIVariables.block_size * y
+                self._draw_block(dx, dy, UIVariables.t_color[int(board[x][y])])
 
     # Draw score bar for one player
-    def draw_score(self, x_start, y_start, player_id):
+    def _draw_score(self, x_start, y_start, player_id):
         # Draw sidebar
         pygame.draw.rect(
             self.screen,
             UIVariables.white,
-            pygame.Rect(x_start, y_start, x_start + 96, y_start + 400)
+            pygame.Rect(x_start, y_start, x_start + 300, y_start + 400)
         )
 
         # Draw texts
+        text_player = UIVariables.h4.render("Player "+str(player_id+1), 1, UIVariables.black)
         text_hold = UIVariables.h5.render("HOLD", 1, UIVariables.black)
         text_next = UIVariables.h5.render("NEXT", 1, UIVariables.black)
         text_score = UIVariables.h5.render("SCORE", 1, UIVariables.black)
         score_value = UIVariables.h4.render(str(self.global_state.engines[player_id].score),
                                             1, UIVariables.black)
-        text_lines = UIVariables.h5.render("Total cleared lines", 1, UIVariables.black)
+        text_lines = UIVariables.h5.render("Cleared lines", 1, UIVariables.black)
         lines_value = UIVariables.h4.render(str(self.global_state.engines[player_id].total_cleared_lines),
                                             1, UIVariables.black)
         text_ko = UIVariables.h5.render("KO's", 1, UIVariables.black)
@@ -119,6 +116,7 @@ class GUI:
                                          1, UIVariables.black)
 
         # Place texts
+        self.screen.blit(text_player, (x_start + 150, y_start + 15))
         self.screen.blit(text_hold, (x_start + 15, y_start + 14))
         self.screen.blit(text_next, (x_start + 15, y_start + 104))
         self.screen.blit(text_score, (x_start + 15, y_start + 194))
@@ -129,8 +127,9 @@ class GUI:
         self.screen.blit(ko_value, (x_start + 20, y_start + 330))
 
     # Draw next mino for one player
-    def draw_next_mino(self, x_start, y_start, player_id):
-        next_mino = self.global_state.engines[player_id].next_shape
+    def _draw_next_mino(self, x_start, y_start, player_id):
+        next_mino_name = self.global_state.engines[player_id].next_shape_name
+        next_mino = UIVariables.shape_names.index(next_mino_name)
         grid_n = Tetrimino.mino_map[next_mino][0]
         for i in range(4):
             for j in range(4):
@@ -144,7 +143,7 @@ class GUI:
                     )
 
     # Draw hold mino for one player
-    def draw_hold_mino(self, x_start, y_start, player_id):
+    def _draw_hold_mino(self, x_start, y_start, player_id):
         if self.global_state.engines[player_id].holded:
             hold_mino = self.global_state.engines[player_id].hold_shape
             grid_h = Tetrimino.mino_map[hold_mino][0]
@@ -160,33 +159,35 @@ class GUI:
                         )
 
     # Draw game screen of one player
-    def draw_one_screen(self, x_start, y_start, player_id):
+    def _draw_one_screen(self, x_start, y_start, player_id):
+        # Draw score bar
+        self._draw_score(x_start, y_start, player_id)
+
+        # Draw next mino
+        self._draw_next_mino(x_start, y_start, player_id)
+
+        # Draw hold mino
+        self._draw_hold_mino(x_start, y_start, player_id)
+
+        # Draw board
+        self._draw_board(x_start, y_start, player_id)
+
+    # Render all players screen
+    def _draw_all_screen(self):
         # Background gray
         self.screen.fill(UIVariables.grey_1)
 
-        # Draw score bar
-        self.draw_score(x_start, y_start, player_id)
-
-        # Draw next mino
-        self.draw_next_mino(x_start, y_start, player_id)
-
-        # Draw hold mino
-        self.draw_hold_mino(x_start, y_start, player_id)
-
-        # Draw board
-        self.draw_board(x_start, y_start, player_id)
-
-    # Render all players screen
-    # Limited to 2 players right now
-    def draw_all_screen(self):
-        if self.global_state.player_num != 2:
+        if self.global_state.player_num > 4:
+            print("Max player number 4")
             pygame.quit()
             exit(10)
+
         for t in range(0, self.global_state.player_num):
-            self.draw_one_screen(t*400, 0, t)
+            self._draw_one_screen(t*300, 0, t)
+        pygame.display.flip()
 
     # TODO
-    def draw_statistics(self):
+    def _draw_statistics(self):
                 return
 
     def draw_whole_screen(self):
@@ -197,7 +198,7 @@ class GUI:
                     self.done = True
                 elif event.type == pygame.USEREVENT:
                     pygame.time.set_timer(pygame.USEREVENT, 300)
-                    self.draw_all_screen()
+                    self._draw_all_screen()
 
                     pause_text = UIVariables.h2_b.render("PAUSED", 1, UIVariables.white)
                     pause_start = UIVariables.h5.render("Press esc to continue", 1, UIVariables.white)
@@ -220,20 +221,11 @@ class GUI:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True
-                elif event.type == pygame.USEREVENT:
-                    # Set speed
-                    if not self.done:
-                        keys_pressed = pygame.key.get_pressed()
-                        if keys_pressed[pygame.K_DOWN]:
-                            pygame.time.set_timer(pygame.USEREVENT, UIVariables.framerate * 1)
-                        else:
-                            pygame.time.set_timer(pygame.USEREVENT, UIVariables.framerate * 10)
                 elif event.type == pygame.KEYDOWN:
                     keys_pressed = pygame.key.get_pressed()
                     if self.global_state.gui_input != '-':
                         self.global_state.gui_input = keys_pressed
-            self.draw_all_screen()
-            pygame.display.update()
+            self._draw_all_screen()
 
         # Game over screen
         elif self.game_over:
@@ -243,7 +235,7 @@ class GUI:
                 elif event.type == pygame.USEREVENT:
                     pygame.time.set_timer(pygame.USEREVENT, 300)
 
-                    self.draw_statistics()
+                    self._draw_statistics()
 
                     # Show "Game over"
                     over_text_1 = UIVariables.h2_b.render("GAME", 1, UIVariables.white)
@@ -262,7 +254,76 @@ class GUI:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
 
+    # Updates the whole screen on call
+    def update_screen(self):
+        self._draw_all_screen()
+
+    # Get last keyboard input since last calling this function
+    def last_gui_input(self):
+        # Pause screen
+        if self.pause:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+                elif event.type == pygame.USEREVENT:
+                    pygame.time.set_timer(pygame.USEREVENT, 300)
+                    self._draw_all_screen()
+
+                    pause_text = UIVariables.h2_b.render("PAUSED", 1, UIVariables.white)
+                    pause_start = UIVariables.h5.render("Press esc to continue", 1, UIVariables.white)
+
+                    self.screen.blit(pause_text, (43, 100))
+                    if self.blink:
+                        self.screen.blit(pause_start, (40, 160))
+                        self.blink = False
+                    else:
+                        self.blink = True
+                    pygame.display.update()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.pause = False
+                        self.global_state.pause = False
+                        pygame.time.set_timer(pygame.USEREVENT, 1)
+
+        # Game screen
+        elif self.start:
+            # Clear event stack and get the last one
+            events = pygame.event.get()
+            for event in reversed(events):
+                if event.type == pygame.KEYDOWN:
+                    keys_pressed = pygame.key.get_pressed()
+                    return keys_pressed
+
+        # Game over screen
+        elif self.game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+                elif event.type == pygame.USEREVENT:
+                    pygame.time.set_timer(pygame.USEREVENT, 300)
+
+                    self._draw_statistics()
+
+                    # Show "Game over"
+                    over_text_1 = UIVariables.h2_b.render("GAME", 1, UIVariables.white)
+                    over_text_2 = UIVariables.h2_b.render("OVER", 1, UIVariables.white)
+                    self.screen.blit(over_text_1, (58, 75))
+                    self.screen.blit(over_text_2, (62, 105))
+
+                    over_start = UIVariables.h5.render("Press return to exit", 1, UIVariables.white)
+                    if self.blink:
+                        self.screen.blit(over_start, (32, 195))
+                        self.blink = False
+                    else:
+                        self.blink = True
+                    pygame.display.update()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+
+
+def gui_start(start_gui):
     while True:
         # Update the gui depending on the framerate
         pygame.time.wait(UIVariables.framerate)
-        draw_whole_screen()
+        start_gui.draw_whole_screen()
