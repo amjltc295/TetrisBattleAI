8431e654d8b66af8451d92b64fe5a2fdfd977f6b
k123321141 <kk123321141@gmail.com>
2018-12-18 18:00:03 +0800

    remove drop

--- src/dqn_agent.py
+++ src/dqn_agent.py
@@ -142,32 +142,21 @@ class CNN_lay(nn.Module):
 
     def __init__(self):
         super(CNN_lay, self).__init__()
-        self.drop = nn.Dropout2d(0.05)
         self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(16)
-#         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-#         self.bn3 = nn.BatchNorm2d(64)
         
         self.conv_collapse = nn.Conv2d(16, 32, kernel_size=(1,20), stride=1)
         self.bn4 = nn.BatchNorm2d(32)
         
         self.conv5 = nn.Conv2d(32, 32, kernel_size=(3,1), stride=1, padding=(1,0))
         self.bn5 = nn.BatchNorm2d(32)
-#         self.conv6 = nn.Conv2d(128, 128, kernel_size=(1,1), stride=1, padding=(0,0))
-#         self.bn6 = nn.BatchNorm2d(128)
-#         self.conv7 = nn.Conv2d(128, 128, kernel_size=(3,1), stride=1, padding=(1,0))
-#         self.bn7 = nn.BatchNorm2d(128)
-        
     def forward(self, x):
-        x = F.relu(self.drop(self.bn1(self.conv1(x))))
-        x = F.relu(self.drop(self.bn2(self.conv2(x))))
-#         x = F.relu(self.drop(self.bn3(self.conv3(x))))
-        x = F.relu(self.drop(self.bn4(self.conv_collapse(x))))
-        x = F.relu(self.drop(self.bn5(self.conv5(x))))
-#         x = F.relu(self.drop(self.bn6(self.conv6(x))))
-#         x = F.relu(self.drop(self.bn7(self.conv7(x))))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn4(self.conv_collapse(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
         return x
 
 ######################################################################
