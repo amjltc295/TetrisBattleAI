0c20e5b62ddcb970edcc026557a8083ed68d9516
YaLiangChang <b03901014@ntu.edu.tw>
2018-12-19 17:14:51 +0800

    Add FixedPolicyAgent in fixed_policy_agent to return the action from the action sequence based on heuristic

--- src/fixed_policy_agent.py
+++ src/fixed_policy_agent.py
@@ -9,14 +9,24 @@ width, height = 10, 20  # standard tetris friends rules
 engine = TetrisEngine(width, height)
 
 
-def select_action(engine, shape, anchor, board):
-    action_final_location_map = engine.get_valid_final_states(shape, anchor, board)
-    act_pairs = [(k, v[2]) for k, v in action_final_location_map.items()]
-    placements = [v for k, v in act_pairs]
-    h_score = [heuristic_fn(s, complete_line(s)) for s in placements]
-    act_idx = np.argmax(h_score)
-    act, placement = act_pairs[act_idx]
-    return act, placement
+class FixedPolicyAgent:
+    def __init__(self):
+        self.current_actions = []
+
+    def get_action(self, engine, shape, anchor, board):
+        if len(self.current_actions) == 0:
+            _, _, self.current_actions = self.select_action(engine, shape, anchor, board)
+        action = self.current_actions.pop(0)
+        return action
+
+    def select_action(self, engine, shape, anchor, board):
+        actions_name_final_location_map = engine.get_valid_final_states(shape, anchor, board)
+        act_pairs = [(k, v[2], v[3]) for k, v in actions_name_final_location_map.items()]
+        placements = [p for k, p, actions in act_pairs]
+        h_score = [heuristic_fn(s, complete_line(s)) for s in placements]
+        act_idx = np.argmax(h_score)
+        actions_name, final_placement, actions = act_pairs[act_idx]
+        return actions_name, final_placement, actions
 
 
 def print_placement(state):
@@ -25,6 +35,7 @@ def print_placement(state):
     print(s)
 
 
+agent = FixedPolicyAgent()
 if __name__ == '__main__':
     # Check if user specified to resume from a checkpoint
     start_epoch = 0
@@ -36,9 +47,9 @@ if __name__ == '__main__':
         cl = 0
         for t in count():
             # Select and perform an action
-            action, placement = select_action(engine, engine.shape, engine.anchor, engine.board)
+            actions_name, placement, actions = agent.select_action(engine, engine.shape, engine.anchor, engine.board)
             # Observations
-            state, reward, done, cleared_lines = engine.step_to_final(action)
+            state, reward, done, cleared_lines = engine.step_to_final(actions_name)
             # Accumulate reward
             score += reward
             cl += cleared_lines
