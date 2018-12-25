b0876120ca39e9f0287f4e920efd77292f64fba3
k123321141 <kk123321141@gmail.com>
2018-12-25 19:47:13 +0800

    remove TetrisAi.py

--- src/Util.py
+++
@@ -1,7 +0,0 @@
-def getGap( intVector ):
-    return [ abs( intVector[i] - intVector[i - 1] ) for i in range( 1, len( intVector ) ) ]
-
-def getDeviation( intVector ):
-    avg = sum( intVector) / len( intVector )
-    deviation = sum( ( aInt - avg )**2 for aInt in intVector )
-    return deviation
