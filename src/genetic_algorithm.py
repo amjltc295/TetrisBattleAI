97c60b7e67d72fcf938f4d2566e16eaa58edd6c8
Patrick Alexander Gersch <p.gersch@campus.tu-berlin.de>
2018-12-26 12:03:57 +0800

    Changed from Rejection sampling to to better pool selection algorithm

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -2,27 +2,33 @@ import random
 from itertools import count
 import numpy as np
 from engine import TetrisEngine
-from genetic_heuristic import gen_heuristic
+from genetic_policy_agent import GeneticPolicyAgent
 
-genes = ['holes_stack_area', 'holes_clean_area', 'height_stack_area',
-         'height_clean_area', 'blocked_lines', 'num_stack_area',
-         'enemy_blocked_lines']
+genes = ['holes_stack_area', 'holes_clean_area', 'height_stack_area', 'height_clean_area',
+         'aggregation_stack_area', 'aggregation_clean_area', 'clear_lines']
+#        , 'blocked_lines', 'num_stack_area',
+#         'enemy_blocked_lines']
 
-init_random_value = 10.0
+init_random_value = 1.0
 
 width, height = 10, 20  # standard tetris friends rules
+
+genetic_agent = GeneticPolicyAgent()
 
 
 class DNA:
     def __init__(self, mutation_rate, engine):
         self.dict_genes = dict()
-        self.dict_genes['holes_stack_area'] = random.uniform(0.0, init_random_value)
-        self.dict_genes['holes_clean_area'] = random.uniform(0.0, init_random_value)
+        self.dict_genes['holes_stack_area'] = random.uniform(-init_random_value, 0.0)
+        self.dict_genes['holes_clean_area'] = random.uniform(-init_random_value, 0.0)
         self.dict_genes['height_stack_area'] = random.uniform(0.0, init_random_value)
-        self.dict_genes['height_clean_area'] = random.uniform(0.0, init_random_value)
-        self.dict_genes['blocked_lines'] = random.uniform(0.0, init_random_value)
-        self.dict_genes['num_stack_area'] = random.uniform(0.0, init_random_value)
-        self.dict_genes['enemy_blocked_lines'] = random.uniform(0.0, init_random_value)
+        self.dict_genes['height_clean_area'] = random.uniform(-init_random_value, init_random_value)
+        self.dict_genes['aggregation_stack_area'] = random.uniform(0.0, init_random_value)
+        self.dict_genes['aggregation_clean_area'] = random.uniform(-init_random_value, init_random_value)
+        self.dict_genes['clear_lines'] = random.uniform(0.0, init_random_value)
+        # self.dict_genes['blocked_lines'] = random.uniform(-init_random_value, init_random_value)
+        # self.dict_genes['num_stack_area'] = random.uniform(-init_random_value, init_random_value)
+        # self.dict_genes['enemy_blocked_lines'] = random.uniform(-init_random_value, init_random_value)
 
         self.mutation_rate = mutation_rate
         self.engine = engine
@@ -35,37 +41,27 @@ class DNA:
         return dna_str
 
     def calculate_fitness(self):
-        state = engine.clear()
-        cl = 0
-        score = 0
-        for t in count():
-            # Select and perform an action
-            actions_name, placement, actions = self.select_action(
-                self.engine, self.engine.shape, self.engine.anchor, self.engine.board)
-            # Observations
-            state, reward, done, cleared_lines = engine.step_to_final(actions_name)
-            # Perform one step of the optimization (on the target network)
-            cl += cleared_lines
-            score += reward
-            print(engine)
-            if done:
-                # Evaluate this DNA
-                print(engine)
-                self.fitness = cl * 1000 + t
-                break
-
-    def select_action(self, engine, shape, anchor, board):
-        # All possible final states
-        actions_name_final_location_map = engine.get_valid_final_states(shape, anchor, board)
-        # act_pairs = (dict_key, final_board, actions)
-        act_pairs = [(k, v[2], v[3]) for k, v in actions_name_final_location_map.items()]
-        # Only final boards
-        placements = [p for k, p, actions in act_pairs]
-        # Uses the heuristic for every possible placement
-        h_score = [gen_heuristic(s, self.dict_genes) for s in placements]
-        act_idx = np.argmax(h_score)
-        actions_name, final_placement, actions = act_pairs[act_idx]
-        return actions_name, final_placement, actions
+        total_score = 0
+        num_games = 3
+        for i in range(num_games):
+            state = engine.clear()
+            cl = 0
+            score = 0
+            for t in count():
+                # Select and perform an action
+                actions_name, placement, actions = genetic_agent.select_action(
+                    self.engine, self.engine.shape, self.engine.anchor, self.engine.board, self.dict_genes)
+                # Observations
+                state, reward, done, cleared_lines = engine.step_to_final(actions_name)
+                # Perform one step of the optimization (on the target network)
+                cl += cleared_lines
+                score += reward
+                # print(engine)
+                if done:
+                    # Evaluate this DNA
+                    total_score = cl * 10 + 3
+                    break
+        self.fitness = int(total_score / num_games)
 
     def make_sexy_baby(self, parent2):
         baby = DNA(self.mutation_rate, self.engine)
@@ -81,7 +77,7 @@ class DNA:
         # Mutation
         for i in range(len(genes)):
             if random.random() < self.mutation_rate:
-                baby.dict_genes[genes[i]] = random.uniform(0.0, init_random_value)
+                baby.dict_genes[genes[i]] = random.uniform(-init_random_value, init_random_value)
 
         return baby
 
@@ -105,7 +101,6 @@ class Population:
         for i in range(self.population_size):
             self.population[i].calculate_fitness()
             total += self.population[i].fitness
-            print("Fitness of DNA ", i, ": ", self.population[i].fitness)
             if self.population[i].fitness > best.fitness:
                 best = self.population[i]
         self.best = best
@@ -114,18 +109,18 @@ class Population:
             self.population[i].prob = self.population[i].fitness / total
 
     def _generate_child(self):
-        random_DNA1 = self.population[random.randint(0, self.population_size - 1)]
-        random_DNA2 = self.population[random.randint(0, self.population_size - 1)]
-        random_prob1 = random.random()
-        random_prob2 = random.random()
-        while random_DNA1.prob > random_prob1 or random_DNA2.prob > random_prob2:
-            if random_DNA1.prob > random_prob1:
-                random_DNA1 = self.population[random.randint(0, self.population_size - 1)]
-                random_prob1 = random.random()
-            if random_DNA2.prob > random_prob2:
-                random_DNA2 = self.population[random.randint(0, self.population_size - 1)]
-                random_prob2 = random.random()
-        return random_DNA1.make_sexy_baby(random_DNA2)
+        parent1 = self.pick_random_child()
+        parent2 = self.pick_random_child()
+        return parent1.make_sexy_baby(parent2)
+
+    def pick_random_child(self):
+        selected_DNA = 0
+        random_prob = random.random()
+        while random_prob > 0:
+            DNA = self.population[selected_DNA]
+            random_prob -= DNA.prob
+            selected_DNA += 1
+        return self.population[selected_DNA-1]
 
     def get_avg_fitness(self):
         avg_fitness = 0.0
@@ -133,6 +128,15 @@ class Population:
             avg_fitness += self.population[i].fitness
         avg_fitness = avg_fitness / self.population_size
         return avg_fitness
+
+    def get_diversity(self):
+        diversity = list()
+        for g in range(len(genes)):
+            total = 0
+            for i in range(self.population_size):
+                total += self.population[i].dict_genes[genes[g]]
+            diversity.append(total/self.population_size)
+        return diversity
 
     def generate_next_generation(self):
         next_generation = list()
@@ -156,11 +160,14 @@ class GeneticAlgorithm:
         for i in range(self.num_generations):
             self.population.generate_next_generation()
             self.population.calc_fitness_prob()
-            print("Max fitness of generation ", self.population.current_generation,
-                  " is ", self.population.max_fitness)
+            print("######################################################")
+            print("Generation ", self.population.current_generation)
+            print("Max fitness: ", self.population.max_fitness)
+            print("Average fitness: ", self.population.get_avg_fitness())
+            print(self.population.get_diversity())
 
 
 if __name__ == '__main__':
     engine = TetrisEngine(width, height, enable_KO=False)
-    darwin = GeneticAlgorithm(100, 0.01, 50, engine)
+    darwin = GeneticAlgorithm(100, 0.01, 1, engine)
     darwin.evolve_the_beasts()
