b16eb4f971e15f5dae0008646b097400ec554c8e
YaLiangChang <b03901014@ntu.edu.tw>
2019-01-05 17:53:33 +0800

    Add new heuristic for rule_based_agent

--- src/heuristic.py
+++ src/heuristic.py
@@ -60,3 +60,22 @@ def complete_line(state):
 
 def heuristic_fn(state, cleared_lines):
     return -0.51*aggregate_height(state) + 0.76*cleared_lines - 0.36*get_hole(state) - 0.18*bumpiness(state)
+
+
+def get_emptyness(state):
+    emptyness = 0
+    width, height = state.shape
+    for y in reversed(range(height)):
+        emptyness += (width - 1 - sum(state[:, y])) * y / 10
+    return emptyness
+
+
+def heuristic_fn2(state, cleared_lines, block, combo=False):
+    height_0 = get_height(state, 0)
+    if combo and get_height(state, 0) > 0 and cleared_lines > 0:
+        return 10000
+    elif cleared_lines > 2:
+        return 10000
+    else:
+        # return -get_emptyness(state) - height_0 * 100 - get_hole(state)
+        return -0.51*aggregate_height(state) + 0.76*cleared_lines - 0.36*get_hole(state) - 0.18*bumpiness(state) - height_0 * 100
